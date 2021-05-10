042bba351a774092327e580c9f236e7a023f8c6d
Maximilian Staab <maximilian.staab.de@gmail.com>
2021-05-10 16:48:50 +0200

    Switched telegram api

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,33 @@ pub mod trash_dates;
 use anyhow::Error;
 use log::info;
 use std::env;
-use teloxide::{prelude::*, utils::command::BotCommand};
-
-#[derive(BotCommand)]
-#[command(rename = "lowercase", description = "Welcome to the Trash-Bot!")]
-enum Command {
-    #[command(description = "Check tomorrows trash dates.")]
-    Trash,
-    #[command(description = "Display this text.")]
-    Help,
-}
-
-async fn answer(cx: UpdateWithCx<AutoSend<Bot>, Message>, command: Command) -> Result<(), Error> {
-    let no_trash_message = String::from("No trash tomorrow!");
-
-    match command {
-        Command::Trash => {
-            let response = match trash_dates::get_tomorrows_trash().await {
-                Ok(t) => match t[..] {
-                    [] => no_trash_message,
-                    _ => t.iter().map(ToString::to_string).collect(),
-                },
-                Err(_) => no_trash_message,
-            };
-            cx.answer(response).await?;
-        }
-        Command::Help => cx.answer(Command::desctiptions()).await?,
-    }
-
-    Ok(())
-}
 
 async fn run() {
-    let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-    teloxide::enable_logging!();
-
     info!("Starting bot.");
+    use carapax::Api;
 
-    let bot = teloxide::prelude::Bot::new(token).auto_send();
-    let bot_name: String = String::from("fluxinator");
+    let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+    let api = Api::new(token).unwrap();
 
-    teloxide::commands_repl(bot, bot_name, answer).await;
+    let no_trash_message: String = String::from("No trash tomorrow!");
+
+    use carapax::Dispatcher;
+
+    let mut dispatcher = Dispatcher::new(api.clone());
+    use carapax::{handler, types::Command, types::Message, HandlerResult};
+
+    #[handler(command = "/start")]
+    async fn command_handler(_context: &Api, _command: Command) {}
+
+    dispatcher.add_handler(command_handler);
+
+    let response = match trash_dates::get_tomorrows_trash().await {
+        Ok(t) => match t[..] {
+            [] => no_trash_message,
+            _ => t.iter().map(ToString::to_string).collect(),
+        },
+        Err(_) => no_trash_message,
+    };
 
     // while let Some(update) = stream.next().await {
     //     let update = update?;
