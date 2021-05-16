4d9849373cbd50fa272be37c1efea91048699a41
Maximilian-Staab <maximilian.staab@mailbox.org>
2021-05-16 21:45:34 +0200

    Add query for requesting dates for specific users

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,65 @@
 use std::env;
 
-use carapax::Api;
+use anyhow::Error;
 use carapax::methods::SendMessage;
+use carapax::Api;
 use log::info;
+use std::fmt::Display;
+use std::future::Future;
+use std::ops::{Index, RangeBounds, RangeFull};
+use std::process::Output;
+use std::slice::SliceIndex;
 use tokio::time::Duration;
+use trash_bot::trash_dates::TrashDate;
 
 pub mod trash_dates;
 
-// static TOKEN: &str = "489706166:AAH2D80-MdEmwKiUIfUXty1L3bhQsf15Wbc";
-// static TOKEN: String = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-// static API: Api = Api::new(TOKEN).unwrap();
-
-// async fn send_message(api: &'static Api, chat_id: Integer) {
-//     let mut scheduler = AsyncScheduler::with_tz(chrono_tz::Europe::Berlin);
-//     scheduler
-//         .every(1.minutes())
-//         .run(|| async { api.execute(SendMessage::new(chat_id, get_message().await)); });
-// }
-
-async fn get_message() -> String {
+async fn get_message<T>(request_future: impl Future<Output = Result<Vec<T>, Error>>) -> String
+where
+    T: ToString,
+{
     let no_trash_message: String = String::from("No trash tomorrow!");
 
-    match trash_dates::get_tomorrows_trash().await {
-        Ok(t) => match t[..] {
-            [] => no_trash_message,
-            _ => t.iter().map(ToString::to_string).collect(),
-        },
+    match request_future.await {
+        Ok(t) => {
+            if t.is_empty() {
+                no_trash_message
+            } else {
+                t.iter().map(ToString::to_string).collect()
+            }
+        }
         Err(_) => no_trash_message,
+    }
+}
+
+async fn get_users() {}
+
+async fn perform_tasks() {
+    let request_performer = trash_dates::RequestPerformer::from_env();
+
+    let users: Vec<trash_dates::User> = match request_performer.get_active_users().await {
+        Ok(t) => t,
+        Err(e) => {
+            log::warn!("No users with active notifications found: {}", e);
+            return;
+        }
+    };
+
+    for user in users {
+        tokio::spawn(async move {
+            let request_performer = trash_dates::RequestPerformer::from_env();
+
+            let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+            let api: Api = Api::new(token).unwrap();
+
+            let future_result = request_performer.get_tomorrows_trash();
+            api.execute(SendMessage::new(
+                user.client_id,
+                get_message(future_result).await,
+            ))
+            .await
+            .unwrap();
+        });
     }
 }
 
@@ -35,25 +68,17 @@ async fn run() {
 
     use clokwerk::{AsyncScheduler, Job, TimeUnits};
 
-    // use carapax::Dispatcher;
-
-    // send_message(&API, 385363028);
-    // let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-    // let api: Api = Api::new(token).unwrap();
-
     let mut scheduler = AsyncScheduler::with_tz(chrono_tz::Europe::Berlin);
-    scheduler.every(1.day()).at("16:00:00").run(|| async {
-        let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-        let api: Api = Api::new(token).unwrap();
-        api.execute(SendMessage::new(385363028, get_message().await))
-            .await
-            .unwrap();
-    });
 
-    loop {
-        scheduler.run_pending().await;
-        tokio::time::sleep(Duration::from_secs(10)).await;
-    }
+    perform_tasks().await;
+    // scheduler.every(1.day()).at("16:00:00").run(perform_tasks);
+    //
+    // loop {
+    //     scheduler.run_pending().await;
+    //     tokio::time::sleep(Duration::from_secs(10)).await;
+    // }
+
+    // REst
 
     // let mut dispatcher = Dispatcher::new(api.clone());
     // use carapax::{handler, types::Command, types::Message, HandlerResult};
