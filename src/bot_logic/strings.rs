af6eb09d81f70809950485ade30c154e3ad2ce2d
Maximilian Staab <maximilian.staab.de@gmail.com>
2021-06-14 19:38:05 +0200

    Fixed some typos in message strings

--- src/bot_logic/strings.rs
+++ src/bot_logic/strings.rs
@@ -1,18 +1,20 @@
 pub const HOUSE_NUMBER_MESSAGE: &str =
     "Bitte gib deine Hausnummer an (die Entsorgungstermine sind abhängig von der Hausnummer).";
 
-pub const HELP_MESSAGE: &str = "Versuche den vollständigen Namen deiner Straße anzugeben. Ansonsten stelle sicher, dass deine Straße im Abfuhrkallender von Karlsruhe aufgeführt ist.\n\nGib deine Straße ein:";
+pub const HELP_MESSAGE: &str = "Versuche den vollständigen Namen deiner Straße anzugeben. Ansonsten stelle sicher, dass deine Straße im Abfuhrkalender von Karlsruhe aufgeführt ist.\n\nGib deine Straße ein:";
 
 pub const HOUSE_NUMBER_QUESTION_1: &str = "Ist das deine Hausnummer";
 pub const HOUSE_NUMBER_QUESTION_2: &str = "Stelle sicher, dass die Nummer korrekt ist, da sonst möglicherweise keine Entsorgungstermine gefunden werden können.";
 pub const CONFIRM_STREET_AND_NUMBER: &str = "Ist das die korrekte Straße und Hausnummer?";
 
 pub const MESSAGE_SEARCH_COULD_NOT_FIND: &str = "Konnte deine Straße nicht in der Datenbank finden. Bitte gib den Namen deiner Straße ein um Vorschläge anzuzeigen:";
-pub const MESSAGE_CONFIRM_ONE_OF_THE_STREETS: &str = "Ist deine Straße hier aufgefürt?";
+pub const MESSAGE_CONFIRM_ONE_OF_THE_STREETS: &str = "Ist deine Straße hier aufgeführt?";
 pub const MESSAGE_SAVE_LOCATION: &str =
     "Speichere deinen Standort für die Abfrage der Entsorgungstermine.";
-pub const MESSAGE_CONFIRM_ADDRESS_ADDED: &str = "Addresse hinzugefügt!";
-pub const MESSAGE_CHANGE_NOTIFICATION_NEGATIVE: &str = "Konnte Benachrichtigungsstatus nicht finden, hast du deine Strasse und Hausnummer schon hinzugefügt?";
+pub const MESSAGE_CONFIRM_ADDRESS_ADDED: &str = "Adresse hinzugefügt!";
+pub const MESSAGE_ERROR_ADDRESS_ADDED: &str =
+    "Konnte Adresse nicht hinzufügen, versuche es später nochmal!";
+pub const MESSAGE_CHANGE_NOTIFICATION_NEGATIVE: &str = "Konnte Benachrichtigungsstatus nicht finden, hast du deine Straße und Hausnummer schon hinzugefügt?";
 pub const MESSAGE_ENTER_HOUSE_NUMBER: &str =
     "Bitte gib die Hausnummer an, die du verwenden willst:";
 pub const MESSAGE_ENTER_STREET_NAME: &str =
@@ -20,9 +22,9 @@ pub const MESSAGE_ENTER_STREET_NAME: &str =
 pub const MESSAGE_DELETED: &str = "Gelöscht!";
 pub const MESSAGE_NOTHING_HAPPENS: &str = "Ok, nichts passiert!";
 pub const MESSAGE_ASK_SEARCH_MODE: &str =
-    "Willst du deine Addresse selbst eingeben oder willst du sie automatisch finden lassen?";
+    "Willst du deine Adresse selbst eingeben oder willst du sie automatisch finden lassen?";
 pub const MESSAGE_ASK_FOR_MANUAL_ENTRY: &str =
-    "Konte deinen Standort nicht zuordnen, bitte gib deine Addresse manuel ein.";
+    "Konnte deinen Standort nicht zuordnen, bitte gib deine Adresse manuell ein.";
 pub const MESSAGE_ASK_WHAT_USER_WANTS: &str = "Was möchtest du tun?";
 pub const HELLO: &str = "Hallo";
 pub const MESSAGE_NOTIFICATIONS_ACTIVATED: &str = "Benachrichtigungen aktiviert";
