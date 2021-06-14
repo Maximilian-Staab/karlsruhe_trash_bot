db43059b8de9d1001736c7182f5360f0f0a73779
Maximilian Staab <maximilian.staab.de@gmail.com>
2021-06-14 11:50:53 +0200

    Removed no longer needed files

--- tests/graph.rs
+++
@@ -1,9 +0,0 @@
-#[tokio::test]
-async fn test_graphql_query() {
-    println!("Testing");
-    let result = trash_bot::trash_dates::get_tomorrows_trash()
-        .await
-        .expect("No data found.");
-    println!("This is the test result: {:?}", result);
-    assert!(true);
-}
